15817f8f9e0c380fc697cc25f992fdacd9e27005
Xiaochu Wang <98920916+xwang78su@users.noreply.github.com>
2022-08-24 14:38:00 -0700

    Update plot_ols.py

--- plot_ols.py
+++ plot_ols.py
@@ -26,7 +26,7 @@ from sklearn.metrics import mean_squared_error, r2_score
 # Load the diabetes dataset
 diabetes_X, diabetes_y = datasets.load_diabetes(return_X_y=True)
 
-# print(diabetes_X.shape)
+print(diabetes_X.shape)
 
 # Use only one feature
 diabetes_X = diabetes_X[:, np.newaxis, 2]
